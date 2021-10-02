2bbfe44c51be90e7950b9b1fa042cf281b82a92d
sheerien <freemebabaj@gmail.com>
2021-10-02 15:57:47 +0300

    login-cookie-php

--- login_cookie/logout.php
+++ login_cookie/logout.php
@@ -5,7 +5,7 @@ require_once("function.php");
 if (isset($_POST["logout"])) {    
     // session_destroy();
     // var_dump($_POST);
-    setcookie("cookie_user", "login", time() - (3600 * 40) * 30, '/', "localhost", false, true);
+    setcookie("cookie_user", "logout", time() - ((3600 * 40) * 30), '/', "localhost", false, true);
     redirect("login");
 }
 
